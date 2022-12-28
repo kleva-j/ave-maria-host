00a3513eaea2bc6e40f8c53c39c2b9f526b8c430
kleva-j <kasmickleva@gmail.com>
2022-12-28 16:53:57 +0100

    style: update trpc file; group all important docs link

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -8,9 +8,6 @@ import getConfig from 'next/config';
 import type { AppRouter } from 'server/routers/_app';
 import superjson from 'superjson';
 
-// ℹ️ Type-only import:
-// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html#type-only-imports-and-export
-
 const { publicRuntimeConfig } = getConfig();
 
 const { APP_URL, WS_URL } = publicRuntimeConfig;
@@ -20,42 +17,29 @@ function getEndingLink(ctx: NextPageContext | undefined) {
     return httpBatchLink({
       url: `${APP_URL}/api/trpc`,
       headers() {
-        if (ctx?.req) {
-          // on ssr, forward client's headers to the server
-          return {
-            ...ctx.req.headers,
-            'x-ssr': '1',
-          };
-        }
+        if (ctx?.req) return { ...ctx.req.headers, 'x-ssr': '1' };
         return {};
       },
     });
   }
-  const client = createWSClient({
-    url: WS_URL,
-  });
-  return wsLink<AppRouter>({
-    client,
-  });
+  const client = createWSClient({ url: WS_URL });
+  return wsLink<AppRouter>({ client });
 }
 
 /**
  * A set of strongly-typed React hooks from your `AppRouter` type signature with `createReactQueryHooks`.
+ *
+ *  IMPORTANT LINKS
+ * @link https://react-query.tanstack.com/reference/QueryClient
  * @link https://trpc.io/docs/react#3-create-trpc-hooks
+ * @link https://trpc.io/docs/data-transformers
+ * @link https://trpc.io/docs/links
+ * @link https://trpc.io/docs/ssr
  */
 export const trpc = createTRPCNext<AppRouter>({
   config({ ctx }) {
-    /**
-     * If you want to use SSR, you need to use the server's full URL
-     * @link https://trpc.io/docs/ssr
-     */
-
     return {
-      /**
-       * @link https://trpc.io/docs/links
-       */
       links: [
-        // adds pretty logs to your console in development and logs errors in production
         loggerLink({
           enabled: (opts) =>
             (process.env.NODE_ENV === 'development' &&
@@ -64,19 +48,10 @@ export const trpc = createTRPCNext<AppRouter>({
         }),
         getEndingLink(ctx),
       ],
-      /**
-       * @link https://trpc.io/docs/data-transformers
-       */
       transformer: superjson,
-      /**
-       * @link https://react-query.tanstack.com/reference/QueryClient
-       */
       queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
     };
   },
-  /**
-   * @link https://trpc.io/docs/ssr
-   */
   ssr: true,
 });
 
