a3b71e361aa963250e01251f465795df04b1c8cc
kleva-j <kasmickleva@gmail.com>
2022-12-31 13:02:47 +0100

    feat: add route to showcase different components

---
+++ src/pages/showcase.tsx
@@ -0,0 +1,17 @@
+import { Text } from '@components/atoms/Text';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <Text size="1">Hello World</Text>
+      <Text size="2">Hello World</Text>
+      <Text size="3">Hello World</Text>
+      <Text size="4">Hello World</Text>
+      <Text size="5">Hello World</Text>
+      <Text size="6">Hello World</Text>
+      <Text size="7">Hello World</Text>
+      <Text size="8">Hello World</Text>
+      <Text size="9">Hello World</Text>
+    </div>
+  );
+}
