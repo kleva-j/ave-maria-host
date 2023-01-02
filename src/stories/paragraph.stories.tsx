6fc0e72bc2384edcff288619372a6f59db9d549e
kleva-j <kasmickleva@gmail.com>
2023-01-02 18:20:22 +0100

    feat: add story for the paragraph component

---
+++ src/stories/paragraph.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Paragraph } from '../components/atoms/Paragraph';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+const Template: ComponentStory<typeof Paragraph> = (args) => (
+  <Paragraph {...args} />
+);
+
+export const ParagraphTemplate = Template.bind({});
+ParagraphTemplate.args = {
+  size: '1',
+  gradient: false,
+  variant: 'blue',
+  children: 'Paragraph',
+};
+
+export default {
+  title: 'Component/Atoms/Paragraph',
+  component: Paragraph,
+  argTypes: {
+    size: { control: 'select', options: ['1', '2'] },
+    variant: {
+      control: 'select',
+      options: [
+        'red',
+        'crimson',
+        'pink',
+        'purple',
+        'violet',
+        'indigo',
+        'blue',
+        'cyan',
+        'teal',
+        'green',
+        'lime',
+        'yellow',
+        'orange',
+        'gold',
+        'bronze',
+        'gray',
+        'contrast',
+      ],
+    },
+    gradient: { control: 'select', options: [true, false] },
+  },
+} as ComponentMeta<typeof Paragraph>;
