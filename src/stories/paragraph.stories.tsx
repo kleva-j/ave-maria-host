f38225c3b10841f5c84456c77bfbeec6582d8db3
kleva-j <kasmickleva@gmail.com>
2023-01-03 16:53:03 +0100

    feat: update paragraph stories with sample textcontent

--- src/stories/paragraph.stories.tsx
+++ src/stories/paragraph.stories.tsx
@@ -11,9 +11,9 @@ ParagraphTemplate.args = {
   size: '1',
   gradient: false,
   variant: 'blue',
-  children: 'Paragraph',
+  children:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit expedita sint dolorem veniam laborum dolorum minima odit. Nihil soluta corporis reprehenderit molestias suscipit quos esse repellat sed cumque est.',
 };
-
 export default {
   title: 'Component/Atoms/Paragraph',
   component: Paragraph,
