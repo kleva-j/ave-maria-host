4e08c2630392ea8f36a88238917601bebfa6debd
kleva-j <kasmickleva@gmail.com>
2023-01-28 21:36:00 +0100

    feat: implement pages for verify-request and signin

---
+++ src/components/molecules/SignIn.tsx
@@ -0,0 +1,190 @@
+import { GitHubLogoIcon, InstagramLogoIcon } from '@radix-ui/react-icons';
+import { Container } from '@components/atoms/Container';
+import { TextField } from '@components/atoms/TextField';
+import { Separator } from '@components/atoms/Separator';
+import { AuthState, formError, AuthData } from 'types';
+import { useForm, zodResolver } from '@mantine/form';
+import { Heading } from '@components/atoms/Heading';
+import { Button } from '@components/atoms/Button';
+import { Loader } from '@components/atoms/Loader';
+import { Label } from '@components/atoms/Label';
+import { Flex } from '@components/atoms/Flex';
+import { Text } from '@components/atoms/Text';
+import { Grid } from '@components/atoms/Grid';
+import { SignInErrors } from '@utils/errors';
+import { Box } from '@components/atoms/Box';
+import { signIn } from 'next-auth/react';
+import { styled } from 'stitches.config';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { z } from 'zod';
+
+type formData = Omit<AuthData, 'password'>;
+
+const validate = zodResolver(
+	z.object({ email: z.string().email({ message: 'Email is invalid.' }) }),
+);
+
+const nextAuthUrl = process.env.NEXT_PUBLIC_NEXTAUTH_URL ?? '';
+
+const FlexWrapper = styled(Flex, {
+	p: 0,
+	border: 'none',
+	width: '100%',
+	maxWidth: '28rem',
+
+	'@bp1': {
+		p: '$6',
+		borderRadius: '$3',
+		border: '1px solid $slate6',
+	},
+});
+
+const RpButton = styled(Button, {
+	borderRadius: '$2',
+	height: '$6',
+	px: '$3',
+	fontSize: '$3',
+	lineHeight: '$sizes$6',
+
+	'@bp1': {
+		height: '$7',
+		px: '$4',
+		fontSize: '$4',
+		lineHeight: '$sizes$7',
+	},
+});
+
+export const SignIn = () => {
+	const { error: signInError } = useRouter().query;
+	const [loading, setLoading] = useState<boolean>(false);
+
+	const [error, setError] = useState<null | formError>(
+		signInError
+			? {
+					title: 'SignIn Error',
+					message: SignInErrors[signInError as string] ?? SignInErrors.default,
+					code: 403,
+			  }
+			: null,
+	);
+
+	const form = useForm({ initialValues: { email: '' }, validate });
+
+	const handleSubmit =
+		(id: string) =>
+		(signInOptions = {}) =>
+			signIn(id, { callbackUrl: nextAuthUrl, ...signInOptions });
+
+	const handleFormSubmit = async (data: Pick<formData, 'email'>) => {
+		setLoading(true);
+
+		const result = await handleSubmit('email')({ ...data, redirect: false });
+
+		console.log(result);
+
+		if (result?.error) {
+			setError({
+				message: result?.error ?? 'Error sending verification link to mail',
+				title: 'Email Verification Error',
+				code: result?.status,
+			});
+		}
+		if (!result?.error && result?.ok && result?.status === 200)
+			console.log('successfull login');
+		setLoading(false);
+	};
+
+	return (
+		<Container size="1" css={{ height: '100vh' }}>
+			<Grid css={{ height: '100%', placeItems: 'center' }}>
+				<FlexWrapper direction="column" gap="4">
+					<Box>
+						<Heading size="3">Hi, Welcome back! 👋</Heading>
+						<Text size="3" css={{ my: '$2' }}>
+							Sign up / Log in to your account.
+						</Text>
+					</Box>
+					<form onSubmit={form.onSubmit(handleFormSubmit)}>
+						<Flex direction="column" gap="3">
+							<Box>
+								<Label size="3" css={{ mb: '$2' }}>
+									Email Address:
+								</Label>
+								<TextField
+									size="3"
+									type="email"
+									autoComplete="off"
+									placeholder="example@email.com"
+									{...form.getInputProps('email')}
+									{...(form.isDirty('email') && form.errors['email']
+										? { state: 'invalid' }
+										: {})}
+								/>
+							</Box>
+							<RpButton
+								size="md"
+								color="teal"
+								// type="submit"
+								// variant="filled"
+								// css={{ width: '100%' }}
+							>
+								{loading ? (
+									<Box>
+										<Loader size="3" variant="red" rot1 />
+									</Box>
+								) : null}
+								Login
+							</RpButton>
+
+							<Box css={{ position: 'relative', my: '$4' }}>
+								<Text
+									size="3"
+									css={{
+										position: 'absolute',
+										backgroundColor: '$loContrast',
+										mx: 'auto',
+										px: '$2',
+										left: 'calc(50% - 50px)',
+										transform: 'translateY(-45%)',
+									}}
+								>
+									Or Login With
+								</Text>
+								<Separator size="2" w-full />
+							</Box>
+
+							<Flex gap="4">
+								<Button
+									size="md"
+									color="teal"
+									variant="outline"
+									css={{ p: '$3', height: '$6', width: 'max-content', flex: 1 }}
+									onClick={handleSubmit('google')}
+								>
+									<Box css={{ mr: '$1' }}>
+										<InstagramLogoIcon />
+									</Box>
+									Google
+								</Button>
+
+								<Button
+									size="md"
+									color="teal"
+									variant="outline"
+									css={{ p: '$3', height: '$6', width: 'max-content', flex: 1 }}
+									onClick={handleSubmit('github')}
+								>
+									<Box css={{ mr: '$1' }}>
+										<GitHubLogoIcon />
+									</Box>
+									Github
+								</Button>
+							</Flex>
+						</Flex>
+					</form>
+				</FlexWrapper>
+			</Grid>
+		</Container>
+	);
+};
---
+++ src/pages/auth/verify-request.tsx
@@ -0,0 +1,112 @@
+import { useInterval, useMediaQuery, useTimeout } from '@mantine/hooks';
+import { Heading } from '@components/atoms/Heading';
+import { Button } from '@components/atoms/Button';
+import { Text } from '@components/atoms/Text';
+import { Flex } from '@components/atoms/Flex';
+import { Card } from '@components/atoms/Card';
+import { GetServerSideProps } from 'next';
+import { signIn } from 'next-auth/react';
+import { AuthState } from 'types';
+import { useState } from 'react';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+// import img from '../../../public/images/mailbox.png';
+
+const VerifyRequest = ({ email }: any) => {
+	const [seconds, setSeconds] = useState(60);
+	const [loading, setLoading] = useState(false);
+	const [touched, setTouched] = useState(false);
+	const timeout = useTimeout(() => {
+		setTouched(false);
+		interval.stop();
+		setSeconds(60);
+	}, 60000);
+	const interval = useInterval(() => setSeconds((s) => s - 1), 1000);
+
+	const handleResendEmail = async () => {
+		setLoading(true);
+		const { ok, error }: any = await signIn('email', {
+			email,
+			redirect: false,
+			authType: AuthState.login,
+		});
+		if (!ok) console.log('Notification sent');
+		setLoading(false);
+		setTouched(true);
+		timeout.start();
+		interval.start();
+	};
+
+	const matches = useMediaQuery('(min-width: 456px)', true);
+
+	return (
+		<Card
+		// py="$2"
+		// radius="md"
+		// px="md"
+		// style={{ width: '100%', maxWidth: '700px' }}
+		// mx="md"
+		>
+			<Flex align="center" justify="center">
+				<Flex direction="column">
+					{matches ? (
+						<Heading size="3">Verify your email address</Heading>
+					) : (
+						<Heading size="4">Verify your email address</Heading>
+					)}
+					<Text size="2">
+						You will need to verify your email to complete registration.
+					</Text>
+					<Image
+						src="/images/mailbox.png"
+						priority
+						style={{ maxWidth: '70%', height: 'auto', margin: 'auto' }}
+						alt="Verify your email address"
+					/>
+					<Text size="2" css={{ maxWidth: '600px', margin: 'auto' }}>
+						An email has been sent to
+						<Text css={{ display: 'inline', fontWeight: 600 }}>{email}</Text>
+						with a link to verify your account. If you have not received the
+						email after a few minutes, please check your spam folder.
+					</Text>
+
+					<Flex justify="center" css={{ margin: 'auto', my: '5rem' }}>
+						<Button
+							onClick={handleResendEmail}
+							disabled={touched ? true : false}
+							// loading={loading}
+						>
+							{touched ? `Wait (${seconds}) seconds` : 'Resend Email'}
+						</Button>
+						<Link href={`/`} passHref>
+							<Button as="a" variant="subtle">
+								Back to Homepage
+							</Button>
+						</Link>
+					</Flex>
+				</Flex>
+			</Flex>
+		</Card>
+	);
+};
+
+VerifyRequest.pageTitle = 'Verify Email';
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+	const { provider = 'email', type = 'email', email } = query;
+	return {
+		...(!email
+			? {
+					redirect: {
+						permanent: true,
+						destination: '/',
+					},
+			  }
+			: {}),
+		props: { provider, type, email },
+	};
+};
+
+export default VerifyRequest;
