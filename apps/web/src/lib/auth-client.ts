cd3595f669c7e56ff24c90458adb3906ddac4d01
kleva-j <kasmickleva@gmail.com>
2025-10-21 06:35:41 +0100

    feat(access-token issue): suspend popar integration

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
-import { polarClient } from "@polar-sh/better-auth";
+// import { polarClient } from "@polar-sh/better-auth";
 
 export const authClient = createAuthClient({
 	baseURL: import.meta.env.VITE_SERVER_URL,
-	plugins: [polarClient()],
+	// plugins: [polarClient()],
 });
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,10 +1,10 @@
-import { polar, checkout, portal } from "@polar-sh/better-auth";
+// import { polar, checkout, portal } from "@polar-sh/better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { betterAuth } from "better-auth";
 import { expo } from "@better-auth/expo";
 import { db } from "@host/db";
 
-import { polarClient } from "./lib/payments";
+// import { polarClient } from "./lib/payments";
 
 import * as schema from "@host/db/schema/auth";
 
@@ -23,24 +23,24 @@ export const auth = betterAuth({
     },
   },
   plugins: [
-    polar({
-      client: polarClient,
-      createCustomerOnSignUp: true,
-      enableCustomerPortal: true,
-      use: [
-        checkout({
-          products: [
-            {
-              productId: "97a8bc83-6afc-4172-8024-57e430a0fa69",
-              slug: "Basic-Plan", // Custom slug for easy reference in Checkout URL, e.g. /checkout/Basic-Plan
-            },
-          ],
-          successUrl: process.env.POLAR_SUCCESS_URL,
-          authenticatedUsersOnly: true,
-        }),
-        portal(),
-      ],
-    }),
+    // polar({
+    //   client: polarClient,
+    //   createCustomerOnSignUp: true,
+    //   enableCustomerPortal: true,
+    //   use: [
+    //     checkout({
+    //       products: [
+    //         {
+    //           productId: "97a8bc83-6afc-4172-8024-57e430a0fa69",
+    //           slug: "Basic-Plan", // Custom slug for easy reference in Checkout URL, e.g. /checkout/Basic-Plan
+    //         },
+    //       ],
+    //       successUrl: process.env.POLAR_SUCCESS_URL,
+    //       authenticatedUsersOnly: true,
+    //     }),
+    //     portal(),
+    //   ],
+    // }),
     expo(),
   ],
 });
